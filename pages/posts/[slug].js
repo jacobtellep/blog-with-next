933e15d94bef4164062c5901c78112c5e9b2506b
Jake Tellep <jj.tellep@gmail.com>
2022-06-18 19:09:43 -0400

    finished loading in content using util functions

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,35 @@
 import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
 
-const PostDetailPage = () => {
-  return <PostContent />;
+const PostDetailPage = (props) => {
+  return <PostContent post={props.post} />;
+};
+
+export const getStaticPaths = () => {
+  const postFileNames = getPostsFiles();
+
+  const slugs = postFileNames.map((fileName) => {
+    return fileName.replace(/\.md$/, '');
+  });
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps = (context) => {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
 };
 
 export default PostDetailPage;
