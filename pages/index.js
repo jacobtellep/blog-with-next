ac641641a4d879d7666909a867072d24f2e57153
Jake Tellep <jj.tellep@gmail.com>
2022-06-18 17:29:44 -0400

    fetching the data from the mark down files. Next need to fetch the content for the post detail

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,25 @@
 import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
+import { getFeaturedPosts } from '../lib/posts-util';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React Framework for production. It makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering.',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React Framework for production. It makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering.',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React Framework for production. It makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering.',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started With NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is the React Framework for production. It makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering.',
-    date: '2022-02-10',
-  },
-];
-
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </>
   );
+};
+
+export const getStaticProps = () => {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    revalidate: 1800, // might wanna use this for api calls for the stock app
+  };
 };
 
 export default HomePage;
