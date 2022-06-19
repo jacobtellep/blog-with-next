a6a47a5297f1ab994660bfc919dcc1f7f08207e3
Jake Tellep <jj.tellep@gmail.com>
2022-06-18 21:20:31 -0400

    setting up database connection for submitting messages

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,7 +1,9 @@
 // this is how you hit API routes in nextjs
 // use this code for reference for future projects
 
-const handler = (req, res) => {
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -19,15 +21,40 @@ const handler = (req, res) => {
       return;
     }
 
-    // store in a database
-
     const newMessage = {
       email,
       name,
       message,
     };
 
-    console.log(newMessage);
+    //     const { MongoClient, ServerApiVersion } = require('mongodb');
+    //     const uri = "mongodb+srv://jacobtellep:<password>@cluster0.mrhnl.mongodb.net/?retryWrites=true&w=majority";
+    //     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    //     client.connect(err => {
+    //     const collection = client.db("test").collection("devices");
+    //      // perform actions on the collection object
+    //      client.close();
+    // });
+
+    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.mrhnl.mongodb.net/?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    });
+
+    try {
+      client.connect((err) => {
+        const collection = client.db('cluster0').collection('messages');
+        // perform actions on the collection object
+        collection.insertOne(newMessage);
+      });
+    } catch (error) {
+      res.status(500).json({ message: 'could not connect to database' });
+      client.close();
+    }
+
+    client.close();
 
     res
       .status(201)
